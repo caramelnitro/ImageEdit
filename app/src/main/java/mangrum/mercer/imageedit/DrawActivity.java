43654ad9f03c3c4b333c923a8ba3953e012b3e1f
caramelnitro <caramelnitro@yahoo.com>
2019-03-27 14:22:21 -0500

    Image editor Created

---
+++ app/src/main/java/mangrum/mercer/imageedit/DrawActivity.java
@@ -0,0 +1,24 @@
+package mangrum.mercer.imageedit;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class DrawActivity extends View {
+
+    public DrawActivity(Context context) {
+        super(context);
+    }
+
+    public DrawActivity(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        imageDraw();
+    }
+
+    private void imageDraw() {
+        //setup to draw
+    }
+
+
+}
