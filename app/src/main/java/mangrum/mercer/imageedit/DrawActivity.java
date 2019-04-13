6a6df50820e5a630d49224033c25daa59b5b30ba
caramelnitro <caramelnitro@yahoo.com>
2019-04-13 10:19:14 -0500

    new pic background fixed

--- app/src/main/java/mangrum/mercer/imageedit/DrawActivity.java
+++ app/src/main/java/mangrum/mercer/imageedit/DrawActivity.java
@@ -156,6 +156,7 @@ public class DrawActivity extends View {
         //set erase true or false
         erase=isErase;
 
+        paint.setColor(Color.WHITE);
         if(erase)paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
         else paint.setXfermode(null);
     }
